a2e2d749e42aaa473c615646429d96ebfb49d3f0
lrh <837233792@qq.com>
2020-02-18 13:02:57 +0800

    项目初始化

---
+++ src/containers/App/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './style.css';
+
+import Home from '../Home';
+
+const App = function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="" component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+const mapStateToProps = (state, props) => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/containers/Home/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from "react-redux";
+
+const Home = function () {
+  return (
+    <div>
+      Home
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {}
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
